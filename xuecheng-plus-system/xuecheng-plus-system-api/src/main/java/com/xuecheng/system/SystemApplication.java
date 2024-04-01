ce3e300565c13bdca37f2b1fd0df4bd768067803
Your Name <you@example.com>
2024-04-01 22:07:36 +0800

    111

--- xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/SystemApplication.java
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/SystemApplication.java
@@ -3,6 +3,7 @@ package com.xuecheng.system;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -18,6 +19,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SystemApplication.class,args);
+        ConfigurableApplicationContext context = SpringApplication.run(SystemApplication.class, args);
+
+
+
+        String info = context.getEnvironment().getProperty("info");
+        System.err.println(info);
+
+
     }
 }
